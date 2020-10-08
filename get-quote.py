20df337103f91e2ecd437075fc9b93507cf2e1d9
ad <add1u@gmx.com>
2020-10-08 22:59:43 +0100

    random quote bot

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,18 @@
+import random
+
 def primary_line():
-  # print("Keep it logically awesome.")
+    # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-  for quote in quotes:
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    # for quote in quotes:
 
-      print('-------')
-      print(quote)
-  # print(quotes)
+    # print('-------')
+    # print(quote)
+    last = len(quotes) -1
+    rnd = random.randint(0, last)
+    print(quotes[rnd])
 
 if __name__== "__main__":
-  primary_line()
+    primary_line()
